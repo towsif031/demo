e2142e73e9f45e7521b9214ae8422171f846e14c
Towsif Ahmed Omi <towsif031@gmail.com>
2021-04-30 14:09:13 +0600

    update.

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,7 +3,7 @@
 if ('serviceWorker' in navigator) {
 	addEventListener('load', () => {
 		navigator.serviceWorker
-			.register('../../sw.js')
+			.register('/sw.js')
 			.then((reg) => console.log('Service Worker: Registered'))
 			.catch((err) => console.log(`Service Worker: Error: ${err}`));
 	});
